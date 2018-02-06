157800bfeecdb2c38fa2a5db59aef0de20970475
Hugo Germain <hugo.germain@supelec.fr>
2018-02-06 05:08:55 +0000

    Added throttle and steering publishing

--- scripts/manual_driver.py
+++ scripts/manual_driver.py
@@ -9,20 +9,23 @@
 """
 
 import os
-from datetime import timedelta, date
 import time
 import pygame
 import rospy
+from datetime import timedelta, date
 from std_msgs.msg import String
-
+from geometry_msgs.msg import Point
 
 class ManualDriver(object):
 
     controller = None
+    axis_data = None
     button_data = None
+    hat_data = None
     mode = 'manual'
     recording = False 
     recording_folder = None
+    pub = None
 
     def callback(self, data):
 	self.mode = data.data
@@ -38,22 +41,32 @@ class ManualDriver(object):
     	# Initialize ROS Subscriber
 	rospy.init_node('manual_driver', anonymous=True)
     	rospy.Subscriber('mode', String, self.callback)
+        self.pub = rospy.Publisher('cmd', Point, queue_size=1)
 
     # Listen for Controller inputs
     def listen(self):
+
+        if not self.axis_data:
+            self.axis_data = {0:0.0,1:0.0,2:0.0,3:0.0,4:0.0}
+
         if not self.button_data:
             self.button_data = {}
             for i in range(self.controller.get_numbuttons()):
                 self.button_data[i] = False
+
+        if not self.hat_data:
+            self.hat_data = {}
+            for i in range(self.controller.get_numhats()):
+                self.hat_data[i] = (0, 0)
         
 	while not rospy.is_shutdown():
 
 	    if self.mode =='manual':
 
 		for event in pygame.event.get():
-			if event.type == pygame.JOYBUTTONDOWN:
 
-				# ---- Starting Recording ----
+			if event.type == pygame.JOYBUTTONDOWN:
+				# Starting Recording
 				if(event.button==0):
 					rospy.loginfo("Starting recording...")
 					self.recording = True
@@ -61,10 +74,15 @@ class ManualDriver(object):
 					print self.recording_folder
 					# TODO: mkdir, etc...
 
-				# ---- Stopping Recording ----
+				# Stopping Recording
 				elif(event.button==2):
 					rospy.loginfo("Stopping recording...")
 					self.recording = False
+			# Acceleration and Steering
+			elif event.type == pygame.JOYAXISMOTION:
+                    		self.axis_data[event.axis] = round(event.value,2)
+				msg = Point(round(0.5 + self.axis_data[4] / 2.0, 5),round(self.axis_data[0], 5), 0.0)
+				self.pub.publish(msg)
 
 
 if __name__ == "__main__":
