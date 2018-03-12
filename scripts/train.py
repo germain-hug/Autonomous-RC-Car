0ffc1673c2b1b4b02feb7b687aadbd6b7b370c59
germain-hug <hugo.germain@supelec.fr>
2018-03-11 19:16:06 -0700

    Bug fixes

--- scripts/train.py
+++ scripts/train.py
@@ -25,9 +25,9 @@ class Trainer(object):
 		self.Y = []
 		for f in fnames:
 			annotations = genfromtxt(f, delimiter=',', dtype=object)
-			self.Y = np.append(self.Y, [np.array(label, dtype=np.float32)
-				for label in annotations[:,1:]], axis=0)
-		print(self.Y)
+			self.Y.append([np.array(label, dtype=np.float32) for label in annotations[:,1:]])
+		print(np.asarray(self.Y))
+		print(np.asarray(self.Y).shape)
 
 	def train(self):
 		self.prepare_data()
