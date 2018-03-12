11c467921071b049eee67a0b0148806d40f43b9c
germain-hug <hugo.germain@supelec.fr>
2018-03-11 18:06:04 -0700

    Bug fixes

--- scripts/train.py
+++ scripts/train.py
@@ -20,15 +20,15 @@ class Trainer(object):
 
 	def train(self):
 		self.prepare_data()
-        self.model.fit(self.X, self.Y, batch_size=self.batch_size, nb_epoch=self.no_of_epochs)
-        self.model.save("models/model.h5")
+		self.model.fit(self.X, self.Y, batch_size=self.batch_size, nb_epoch=self.no_of_epochs)
+		self.model.save("models/model.h5")
 
 	def prepare_data(self):
 		fnames = glob.glob("/data/*/*.csv")
-        self.X = [], self.Y = []
-        for f in fnames:
-            annotations = genfromtxt(f, delimiter=',')
-            x = np.array([np.array(Image.open(fname)) for fname in annotations[:,0]])
-            y = [np.array(label) for label in annotations[:,1:2]]
-            self.X.append(x)
-            self.Y.append(y)
+		self.X = [], self.Y = []
+		for f in fnames:
+			annotations = genfromtxt(f, delimiter=',')
+			x = np.array([np.array(Image.open(fname)) for fname in annotations[:,0]])
+			y = [np.array(label) for label in annotations[:,1:2]]
+			self.X.append(x)
+			self.Y.append(y)
